0fa45e1e1fe22ba599be1112c44614a92915e5bb
miruky <h.miyoki@gmail.com>
2026-02-20 20:21:06 +0900

    ライブラリの公開APIを集約

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+export { AREA_URL, escapeHtml, forecastUrl, groupOffices, overviewUrl } from './api';
+export { renderTempChart } from './chart';
+export { weatherIcon } from './icons';
+export { listForecastAreas, parseForecast } from './parse';
+export { normalizeText, weatherCategory, weatherLabel } from './weather';
+export type { WeatherCategory } from './weather';
+export type { Dashboard, OfficeGroup, Overview, ShortTermDay, WeeklyDay } from './types';
